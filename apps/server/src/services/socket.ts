8bd21737dfea5f37e9fb7fc4164486fc04d9f6d8
itsmeherepranetha <pranethacoding@gmail.com>
2025-05-29 13:03:27 +0530

    socket provider,socket.io-client, basic frontend

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,7 +4,12 @@ export default class SocketService{
     private _io:Server;
 
     constructor(){
-        this._io=new Server();
+        this._io=new Server({
+            cors:{
+                allowedHeaders:['*'],
+                origin:'*'
+            }
+        });
         console.log("init socket server");
     }
 
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,18 +1,48 @@
 'use client'
 // contexts must be client components
 
-import React from "react";
+import React, { useCallback, useEffect, useContext, useState } from "react";
+import { io, Socket} from "socket.io-client";
 
 interface SocketProviderProps{
     children?:React.ReactNode
 }
 
-const SocketContext=React.createContext(null);
+interface InterfaceSocketContext{
+    sendMessage:(msg:string)=>any;
+}
 
+const SocketContext=React.createContext<InterfaceSocketContext | null>(null);
+
+export const useSocket=()=>{
+    const state=useContext(SocketContext);
+    if(!state)throw new Error('state is undefined');
+    return state;
+}
 
 export const SocketProvider:React.FC<SocketProviderProps>=({children})=>{
+
+    const [socket,setSocket]=useState<Socket>();
+
+    const sendMessage:InterfaceSocketContext['sendMessage']=useCallback((msg)=>{
+        console.log("send message",msg);
+        if(socket){
+            socket.emit('event:message',{message:msg})
+        }
+    },[socket])
+
+    useEffect(()=>{
+        const _socket=io('http://localhost:8000')
+        setSocket(_socket);
+
+        return ()=>{
+            _socket.disconnect();
+            setSocket(undefined);
+        }
+    },[])
+
     return(
-        <SocketContext.Provider value={null}>
+        <SocketContext.Provider value={{sendMessage:sendMessage}}>
             {children}
         </SocketContext.Provider>
     )
